2c1ab2434872b52f9417b1307c40b36b82ea9582
Stephan Rotolante <stephanrotolante@gmail.com>
2025-04-30 13:09:04 -0400

    refactor: add clang formatting

--- example/main.c
+++ example/main.c
@@ -9,21 +9,19 @@
 #define STOP_BITS 1
 #define PARITY UART_PARITY_NONE
 
-
-
 int main() {
 
-    stdio_init_all();
+  stdio_init_all();
+  sleep_ms(1000);
+
+  uart_set_format(UART_ID, DATA_BITS, STOP_BITS, PARITY);
+
+  printf("Starting up...\n");
+
+  while (1) {
+    tight_loop_contents();
+
+    printf("hello there\n");
     sleep_ms(1000);
-
-    uart_set_format(UART_ID, DATA_BITS, STOP_BITS, PARITY);
-
-    printf("Starting up...\n");
-
-    while (1) {
-        tight_loop_contents();
-
-        printf("hello there\n");
-        sleep_ms(1000);
-    }
+  }
 }
